949ffeb48f7399452e9682d2ec145fd56acf03ef
Fabricio <f.cesp21@gmail.com>
2024-09-28 19:28:48 -0600

    rk4.py docstring

--- rk4.py
+++ rk4.py
@@ -1,65 +1,56 @@
-#!/usr/bin/env python
-
 import numpy as np
 
-oOper = np.array([[0, 1], [1, 0]])
-
-print(oOper)
-
-yInit = np.array([[1, 0], [0, 0]])
-
-print(yInit)
-
 def dyn_generator(oper, state):
+    """Generador dinámico de dos ndarrays.
+
+    Esta función calcula el generador dinámico al multiplicar las matrices
+    `oper` y `state`, y restarle la multiplicación en el orden inverso de las
+    mismas. El resultado se multiplica por -1.0j (imaginario).
+
+    Examples:
+        >>> dyn_generator(np.array([[0, 1], [1, 0]]), np.array([[1, 0], [0, 0]]))
+        [[0.-0.j 0.+1.j]
+         [0.-1.j 0.-0.j]]
+
+    Args:
+        oper (ndarray): Primera matriz.
+        state (ndarray): Segunda matriz.
+
+    Returns:
+        ndarray: Devuelve el generador dinámico resultante de la operación.
+
+    """
     return -1.0j * (np.dot(oper, state) - np.dot(state, oper))
 
+print(help(dyn_generator)) 
+
+
 def rk4(func, oper, state, h):
-    k_1 = h*func(oper, state)
-    k_2 = h*func(oper, state+k_1/2)
-    k_3 = h*func(oper, state+k_2/2)
-    k_4 = h*func(oper, state+k_3)
-    return state + 1/6 * (k_1 + 2*k_2 + 2*k_3 + k_4)
+    """Implementación del método Rung-Kutta de orden 4 (RK4) sin una dependencia explícita de la variable temporal.
 
-times = np.linspace(0.0, 10.0, num=500)
+    Examples:
+        >>> rk4(dyn_generator, np.array([[0, 1], [1, 0]]), np.array([[1, 0], [0, 0]]), 0.2)
+        [[0.96053333+0.j        , 0.        +0.19466667j],
+         [0.        -0.19466667j, 0.03946667+0.j        ]]
 
-h = times[1] - times[0]
-# h = 1/(num-1)
+    Args:
+        func (callable): Función que describe la evolución del sistema.
+            Debe aceptar dos argumentos: `oper` y `state`.
+        oper (ndarray): Operador lineal (matriz en este caso) y primer argumento
+            que se pasa a la función `func`.
+        state (ndarray): Estado actual del sistema (matriz en este caso) y
+            segundo argumento que se pasa a la función `func`.
+        h (float): Tamaño del paso que determina el intervalo de tiempo entre
+            los estados calculados.
 
-stateQuant00 = np.zeros(times.size)
-stateQuant11 = np.zeros(times.size)
+    Returns:
+        ndarray: El nuevo estado del sistema después de aplicar el método RK4.
 
-for tt in range(times.size):
-    # Guarde el valor de las entradas (0,0) y (1,1) en los arreglos que definimos
-    # Obtenga estos valores de las entradas de yInit
-    # Código aquí ->
-    stateQuant00[tt] = yInit[0,0].real
-    stateQuant11[tt] = yInit[1,1].real
+    """
+    k_1 = func(oper, state)
+    k_2 = func(oper, state + k_1 / 2)
+    k_3 = func(oper, state + k_2 / 2)
+    k_4 = func(oper, state + k_3)
+    return state + 1 / 6 * h * (k_1 + 2 * k_2 + 2 * k_3 + k_4)
 
-    # Invoque rk4 operando sobre yInit
-    # y devuelva el resultado a un nuevo yN
-    # Código aquí ->
-    yN = rk4(dyn_generator, oOper, yInit, h)
-
-    # Ahora asignamos yN a yInit
-    # De esta manera, en la siguiente iteración, el operador de esta iteración se convierte en el inicial
-    # de la siguiente iteración
-    yInit = yN
-
-
-import matplotlib.pyplot as plt
-
-# Código aquí ->
-t = times
-y_00 = stateQuant00
-y_11 = stateQuant11 
-
-fig, axes = plt.subplots()
-plt.xlabel('Tiempo')
-plt.ylabel('Valor del estado')
-axes.plot(t, y_00, 'b')
-twin_axes = axes.twiny()
-twin_axes.plot(t, y_11, 'r')
-
-plt.show()
-plt.close()
-
+print(help(rk4))
